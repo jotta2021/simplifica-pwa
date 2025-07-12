d499823fdee0087eac54857175df91257637cc1f
jotta2021 <joanderson2572@gmail.com>
2025-07-12 10:25:10 -0300

    Enhance headerTop component with new gradient background and responsive messaging for test version notification.

--- src/app/(protected)/_components/headerTop/headerTop.tsx
+++ src/app/(protected)/_components/headerTop/headerTop.tsx
@@ -23,7 +23,12 @@ const session = await auth.api.getSession({
 
 
   return (
-   <div className="px-6 py-2 dark:bg-neutral-900">
+   <div className="px-6 py-2 dark:bg-neutral-900 flex w-full justify-between gap-10">
+    <div className="bg-gradient-to-r from-green-100 via-green-200 to-green-300  h-full text-sm w-[60%] p-2 rounded-sm text-green-600 font-medium">
+
+<p className="max-md:hidden">Você está utilizando uma versão de teste do Simplifica - Finanças Pessoais</p>
+<p className="hidden max-md:flex">Vs. Teste</p>
+    </div> 
 <Actions notifications={notifications} />
    </div>
   );
